ab0934400c1743df40fcf41f6bc2ac7208136920
montaguethomas <montague.thomas@gmail.com>
2009-08-31 02:40:46 +0000

    Added ci_flickr_api_lang.php to english folder.

---
+++ application/language/english/ci_flickr_api_lang.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['ci_flickr_api_'] = '';
+
+
+/* End of file ci_flickr_api_lang.php */
+/* Location: ./system/application/language/english/ci_flickr_api_lang.php */
